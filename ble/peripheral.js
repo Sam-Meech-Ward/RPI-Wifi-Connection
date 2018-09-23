ffe312c09060bdcf4c4d892bdc70b008e92a0322
Sam Meech-Ward <sam@meech-ward.com>
2018-09-23 14:47:14 -0700

    Add a peripheral file
    this will controll the sesrvices

---
+++ ble/peripheral.js
@@ -0,0 +1,40 @@
+function start(getNetworks, connectToNetwork) {
+
+  const bleno = require('bleno');
+  const WifiNetworksService = require('./WifiNetworksService');
+  const wifiNetworksService = new WifiNetworksService(getNetworks);
+
+  const name = "SamsWifi";
+
+  bleno.on('stateChange', function(state) {
+    if (state === 'poweredOn') {
+      //
+      // We will also advertise the service ID in the advertising packet,
+      // so it's easier to find.
+      //
+      bleno.startAdvertising(name, [wifiNetworksService.uuid], (err) => {
+        if (err) {
+          console.log("Error", err);
+        }
+      });
+    }
+    else {
+      bleno.stopAdvertising();
+    }
+  });
+  
+  bleno.on('advertisingStart', function(err) {
+    if (!err) {
+      console.log('advertising...');
+      //
+      // Once we are advertising, it's time to set up our services,
+      // along with our characteristics.
+      //
+      bleno.setServices([
+        wifiNetworksService
+      ]);
+    }
+  });
+}
+
+exports.start = start;
