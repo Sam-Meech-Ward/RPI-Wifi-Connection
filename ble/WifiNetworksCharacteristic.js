40d93efcc339becfb0ed60cbf3f1cd10029b4b05
Sam Meech-Ward <sam@meech-ward.com>
2018-09-23 14:38:45 -0700

    Add a WifiNetworksCharacteristic

---
+++ ble/WifiNetworksCharacteristic.js
@@ -0,0 +1,45 @@
+const bleno = require('bleno');
+const util = require('util');
+
+/**
+ * 
+ * @param {A function that returns a promise that resolves to an array of network objects that look like {bssid, signalLevel, ssid}} networks 
+ */
+function WifiNetworksCharacteristic(getNetworks) {
+  bleno.Characteristic.call(this, {
+    uuid: 'c40d35b1-9bcd-401f-b5ee-8abf58dab24b',
+    properties: ['read'],
+    descriptors: [
+      new bleno.Descriptor({
+        uuid: '5b12f89f-4af6-43a6-b46d-08f7565243dd',
+        value: 'Gets All of the wifi networks.'
+      })
+    ]
+  });
+
+  this.getNetworks = getNetworks;
+}
+
+module.exports = WifiNetworksCharacteristic;
+
+util.inherits(WifiNetworksCharacteristic, bleno.Characteristic);
+
+PizzaCrustCharacteristic.prototype.onReadRequest = function(offset, callback) {
+  if (offset) {
+    callback(this.RESULT_ATTR_NOT_LONG, null);
+    return;
+  }
+
+  this.getNetworks()
+    .then((networks) => {
+      const bufStr = JSON.stringify(networks);
+      const data = Buffer.from(bufStr, 'utf8');
+      data.writeUInt8(this.pizza.crust, 0);
+      callback(this.RESULT_SUCCESS, data);
+    })
+    .catch((error) => {
+      callback(this.RESULT_UNLIKELY_ERROR, Buffer.from("Couldn't get networks", 'utf8'));
+    });
+};
+
+module.exports = PizzaCrustCharacteristic;
